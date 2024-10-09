bc9a16750e7c6129b2682158bccc0403d977ab6c
vitormanzano <160519411+vitormanzano@users.noreply.github.com>
2024-10-09 19:48:56 -0300

    ok

--- src/accounts/accounts.ts
+++ src/accounts/accounts.ts
@@ -1,32 +1,32 @@
-import {Request, RequestHandler, Response} from "express";
+import { Request, RequestHandler, Response } from "express";
 import OracleDB from "oracledb";
 
 /*
     Nampespace que contém tudo sobre "contas de usuários"
 */
 export namespace AccountsHandler {
-    
+
     /**
      * Tipo UserAccount
      */
     export type UserAccount = {
         id: number | undefined;   //Porque nao chega no bd com o id
-        completeName:string;
-        email:string;
-        password:string | undefined;
-    }; 
+        completeName: string;
+        email: string;
+        password: string | undefined;
+    };
 
-    async function login( email:string, password:string): Promise<UserAccount | undefined>{  //Ou retorna um user ou undefined
+    async function login(email: string, password: string): Promise<UserAccount | undefined> {  //Ou retorna um user ou undefined
         //passo 1 - Conectar se ao oracle.     //Estudar promisse
         let connection = await OracleDB.getConnection({
-            user:"ADMIN",
+            user: "ADMIN",
             password: "123",
             connectString: "Minha string de conexao..."
         });
 
         let results = await connection.execute(   //: Significa que pega da requisicao 
             'SELECT * FROM ACCOUNT WHERE email = :email AND password = :password',
-            [email,password] //Parametros
+            [email, password] //Parametros
         );
 
         console.dir(results.rows);
@@ -40,18 +40,18 @@ export namespace AccountsHandler {
         }
     }
 
-    export const loginHandler:RequestHandler = 
-        async (req:Request, res: Response) => {
+    export const loginHandler: RequestHandler =
+        async (req: Request, res: Response) => {
             const pEmail = req.get('email');
             const pPassword = req.get('password');
 
-            if (pEmail && pPassword){
-                await login(pEmail,pPassword)
+            if (pEmail && pPassword) {
+                await login(pEmail, pPassword)
                 res.statusCode = 200;
                 res.send('Login Realizado... Confira...');
             }
             else {
-                res.statusCode = 400;  
+                res.statusCode = 400;
                 res.send('Requisição inválida - Parâmetros faltnado.');
             }
         }
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
 import express from "express";
-import {Request, Response, Router} from "express";
+import { Request, Response, Router } from "express";
 import { AccountsHandler } from "./accounts/accounts";
 
-const port = 3000; 
+const port = 3000;
 const server = express();
 const routes = Router();
 
 // definir as rotas. 
 // a rota tem um verbo/método http (GET, POST, PUT, DELETE)
-routes.get('/', (req: Request, res: Response)=>{
+routes.get('/', (req: Request, res: Response) => {
     res.statusCode = 403;
     res.send('Acesso não permitido.');
 });
-
-
 
 // vamos organizar as rotas em outro local 
 
@@ -24,6 +22,6 @@ routes.post('/login', AccountsHandler.loginHandler)
 
 server.use(routes);
 
-server.listen(port, ()=>{
+server.listen(port, () => {
     console.log(`Server is running on: ${port}`);
 })
